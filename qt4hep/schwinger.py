e87aa94a2052629a6e83fca1918d56b2eb15339e
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2025-01-23 10:12:21 +0900

    making more functions global + mass*lsp->mu

--- qt4hep/schwinger.py
+++ qt4hep/schwinger.py
@@ -8,10 +8,10 @@ from .sparse import dagger, jw_annihilator_spo, ab_to_phi_sparse
 jax.config.update('jax_enable_x64', True)
 
 
-def setup(num_sites, lsp, mass, l0):
+def setup(num_sites, mu, l0):
     half_lat = num_sites // 2
     hdim = 2 ** num_sites
-    rapidity, wavenumber = get_rapidity(num_sites, lsp, mass, with_wn=True)
+    rapidity, wavenumber = get_rapidity(num_sites, mu, with_wn=True)
 
     print('Identifying Fock-space physical state indices')
     fock_config = (jnp.arange(hdim)[:, None] >> jnp.arange(num_sites)[None, :]) % 2
@@ -22,9 +22,9 @@ def setup(num_sites, lsp, mass, l0):
 
     print('Free Hamiltonian')
     occupancy = fock_config[fock_indices]
-    energy = mass * np.cosh(rapidity)
+    energy = mu * np.cosh(rapidity)
     energy = np.tile(energy, 2)
-    h_free = np.sum(energy[None, :] * occupancy, axis=1)
+    h_free = jnp.sum(energy[None, :] * occupancy, axis=1)
 
     print('Identifying position-space physical state indices')
     fermion_config = fock_config.copy()
@@ -53,30 +53,27 @@ def setup(num_sites, lsp, mass, l0):
 
     h_elec = jnp.einsum('h,ih,jh->ij', l2, basis_change_mat, basis_change_mat.conjugate())
     h_elec = jnp.where(jnp.isclose(h_elec, 0.), 0., h_elec)
-    h_elec = np.array(h_elec)
 
     return fock_indices, pos_indices, fermi_num, basis_change_mat, h_free, h_elec
 
 
 @jax.jit
+def _update_proj(isite, data):
+    proj, bidx, fermi_num = data
+    proj = jax.lax.cond(
+        jnp.equal(bidx[isite], 1),
+        lambda: fermi_num[isite] @ proj,
+        lambda: proj - fermi_num[isite] @ proj
+    )
+    return (proj, bidx, fermi_num)
+
+
+@jax.jit
 def _position_as_fock(idx, fermi_num, proj_init):
-    def occupied(isite, proj):
-        return fermi_num[isite] @ proj
-
-    def unoccupied(isite, proj):
-        return proj - fermi_num[isite] @ proj
-
     bidx = (idx >> jnp.arange(fermi_num.shape[0])) % 2
-    proj = proj_init[bidx[0]]
-    for isite in range(1, fermi_num.shape[0]):
-        proj = jax.lax.cond(
-            jnp.equal(bidx[isite], 1),
-            occupied,
-            unoccupied,
-            isite,
-            proj
-        )
-
+    proj = jax.lax.fori_loop(
+        1, fermi_num.shape[0], _update_proj, (proj_init[bidx[0]], bidx, fermi_num)
+    )[0]
     return jnp.linalg.eigh(proj)[1][:, -1]
 
     # absvals = jnp.sqrt(jnp.diagonal(proj).real)
@@ -88,9 +85,17 @@ def _position_as_fock(idx, fermi_num, proj_init):
     # return absvals * jnp.exp(1.j * jnp.angle(proj[:, ikey]))
 
 
+@jax.jit
+def _position_as_fock_i(icol, data):
+    mat, pos_indices, fermi_num, proj_init = data
+    mat = mat.at[:, icol].set(_position_as_fock(pos_indices[icol], fermi_num, proj_init))
+    return (mat, pos_indices, fermi_num, proj_init)
+
+
 # _position_as_fock_v = jax.jit(jax.vmap(_position_as_fock, in_axes=(0, None, None), out_axes=1))
 
 
+@jax.jit
 def position_states_as_fock_state_sums(pos_indices, fermi_num):
     pos_indices = jnp.asarray(pos_indices)
     subdim = pos_indices.shape[0]
@@ -100,12 +105,9 @@ def position_states_as_fock_state_sums(pos_indices, fermi_num):
         fermi_num[0].todense()
     ])
 
-    @jax.jit
-    def _position_as_fock_i(icol, mat):
-        return mat.at[:, icol].set(_position_as_fock(pos_indices[icol], fermi_num, proj_init))
-
     mat_init = jnp.empty((subdim, subdim), dtype=fermi_num.dtype)
-    return jax.lax.fori_loop(0, subdim, _position_as_fock_i, mat_init)
+    return jax.lax.fori_loop(0, subdim, _position_as_fock_i,
+                             (mat_init, pos_indices, fermi_num, proj_init))[0]
 
     # batch_size = 32
     # num_batches = subdim // batch_size + int(subdim % batch_size != 0)
