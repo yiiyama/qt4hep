423ce16e2bc3a4db3d3e5efcaaa84c6c408f2f49
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2025-01-23 10:47:21 +0900

    using mu again

--- qt4hep/staggered_fermion.py
+++ qt4hep/staggered_fermion.py
@@ -1,13 +1,12 @@
 import numpy as np
 
 
-def get_rapidity(num_sites, lsp, mass, wavenumber=None, with_wn=False, npmod=np):
+def get_rapidity(num_sites, mu, wavenumber=None, with_wn=False, npmod=np):
     if wavenumber is None:
         half_lat = num_sites // 2
         wavenumber = npmod.arange(-half_lat // 2, half_lat // 2)
-    momentum = npmod.sin(2 * npmod.pi / num_sites * wavenumber) / lsp
-    energy = npmod.sqrt(npmod.square(mass) + npmod.square(momentum))
-    rapidity = npmod.log((momentum + energy) / mass)
+    gamma_beta = npmod.sin(2 * npmod.pi / num_sites * wavenumber) / mu
+    rapidity = npmod.arcsinh(gamma_beta)
 
     if with_wn:
         return rapidity, wavenumber
